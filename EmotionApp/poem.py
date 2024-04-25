336c0366e43005d3d1482707d28d787ed97a55a3
Fay Cai <fc1237@nyu.edu>
2024-04-25 00:11:11 -0400

    update story and data

--- EmotionApp/poem.py
+++ EmotionApp/poem.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import random
+import json
 
 
 model_name = 'gpt2'
@@ -9,13 +10,12 @@ model = GPT2LMHeadModel.from_pretrained(model_name)
 
 font_css = """
 <style>
-    p {
+    p {{
         font-family: 'Helvetica Neue', Arial, sans-serif !important;
-        color:#a6a4a4;
+        color:#013605;
         font-weight: regular;
-        
         font-size:18px;
-    }
+    }}
 </style>
 """
 st.markdown(font_css, unsafe_allow_html=True)
@@ -27,6 +27,14 @@ def generate_text(prompt, length=300, temperature=0.65):
     text = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     return text
+
+
+books = json.load(open("books.json"))
+
+celebrities = json.load(open("celebrities.json"))
+
+president_quotes = json.load(open("president_quote.json"))
+
 
 
 Beginning = ["As the leaves started turning golden", "On a rainy Sunday", "On the eve of a great adventure", "It's the most wonderful season of the year", "Under a starlit sky", "During the last days of summer", "When the first snow began to fall", "While the town slept soundly", "On the busiest day of the year"]
@@ -72,7 +80,7 @@ character_traits = {
         "timekeeper",
         "gardener",  
         "journalist", 
-        "scientist"
+        "scientist",
         "merchant", 
         "scholar",  
         "teacher",  
@@ -106,7 +114,7 @@ def app():
     genre = st.sidebar.selectbox("Genre", ["Fantasy", "Horror", "Romantic"])
     main_character = st.sidebar.text_input("Main Character", "Alice")
     secondary_character = st.sidebar.text_input("Secondary Character", "Eleanor")
-    setting = st.sidebar.text_input("Place", "enchanted forest")
+    setting = st.sidebar.text_input("Place", "coffee shop")
     #plot2 =  st.sidebar.text_area("Plot"," ")
 
     story_length = st.sidebar.slider("Story Length", 100, 500, 300)
@@ -117,10 +125,10 @@ def app():
 
     st.title("Welcome to the Electronic Story Generator!")
 
-    for _ in range(4):
+    for _ in range(5):
             st.write("")
 
-    st.write("Dive into the world of bespoke storytelling, this generator leverages the transformer model from Hugging Face. Inspired by tracery-like grammar, it dynamically constructs story frameworks, enabling you to customize narratives in ways that expertly blend structured creativity with elements of surprise. Each story unfolds uniquely based on your input prompt, offering a tailored experience that ignites imagination and fosters innovative storytelling. ")
+    st.write("Dive into the world of bespoke storytelling with our generator, crafted in two distinct parts. First two-thirds of each narrative is meticulously shaped using a tracery-like grammar structure personally curated by me, weaving elements like real book titles, presidential quotes, and celebrity names into the fabric of the storytelling. The remaining third leverages the power of the GPT-2 model to generate the rest, based on the clues from the prompt we collaboratively create in the initial sentences. Each narrative unfolds uniquely based on your input, providing a tailored experience that ignites the imagination and curiosity, fostering innovative storytelling through electronic text and the mathematical algorithms within the AI mind.")
 
 
     if st.sidebar.button("Generate Story"):
@@ -129,23 +137,35 @@ def app():
 
 
 def create_story(genre, main_character, secondary_character, setting, story_length, temperature):
-
-    #plot = plot2
-    #plot = random.choice(plot_elements[genre])  
+    
+    # Random selections for story elements
     Begin = random.choice(Beginning)
-    keywords = random.sample(genre_keywords[genre], 2) 
+    keywords = random.sample(genre_keywords[genre], 2)
 
     selected_personality = random.choice(character_traits["personality"])
     selected_occupation = random.choice(character_traits["occupation"])
-
-
+    selected_book = random.choice(books)
+    selected_book2 = random.choice(books)
+    selected_celebrities = random.choice(celebrities)
+    selected_president = random.choice(president_quotes)
+    selected_name = selected_president['name']
+    selected_quote = random.choice(selected_president['quotes'])
     selected_personality2 = random.choice(character_traits["personality"])
     selected_occupation2 = random.choice(character_traits["occupation"])
-   
-  
 
-    prompt = f"""{Begin}, {main_character}, a {selected_personality} {selected_occupation}, found herself in {setting}.
-        Alongside her was {secondary_character}, a {selected_personality2} {selected_occupation2}. It unfolds with {keywords[0]} and {keywords[1]}, How will their adventure unfold in this magical {setting}? """
+
+    # Generate the prompt
+    prompt = f"""
+{Begin}, {main_character}, a {selected_personality} {selected_occupation}, settled into a quiet nook with the book, '{selected_book}', at the most enchanting {setting} in town. 
+Next to her, {secondary_character}, a {selected_personality2} {selected_occupation2}, was engrossed in '{selected_book2}' at the neighboring nook. 
+Suddenly, a distinct voice in the distance began reciting "{selected_quote}". 
+"Could that possibly be the President {selected_name}?" {main_character} mused, her gaze still fixed on her page. 
+As the voice drew nearer, {main_character} finally looked up to see none other than {selected_celebrities} approaching! 
+This story is all about {keywords[0]} and {keywords[1]}. 
+Why was {selected_celebrities} there, and what adventures await in this magical {setting}?
+"""
+
+
 
     story = generate_text(prompt, length=story_length, temperature=temperature)
         
